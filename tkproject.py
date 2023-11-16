fa014623be76db1714f8748a0ea6aa50494ccfe3
YunYunYY <76467359+YunYunYY@users.noreply.github.com>
2023-11-16 10:20:23 +0900

    Update tkproject.py
    - Add new window
    - Add function check patron ID

--- tkproject.py
+++ tkproject.py
@@ -1,21 +1,41 @@
 import tkinter as tk
-window = tk.Tk()
 
+
+window = tk.Tk()
 window.geometry("600x400+100+100")
 window.resizable(1,1)
+window.attributes('-fullscreen',True)
 
 label=tk.Label(window,text="Test")
 label.pack()
 
-def getTextInput():
-    result=str(textExample.get())
-    #print(result)
-    label.configure(text=result)
+aa=["123","234"]
 
 def showInput(event):
-    result="Patron ID : "+str(textExample.get())[1:12]
+    a=str(textExample.get())[1:12]
+    result="Patron ID : "+a
     label.config(text=result)
+    if a in aa:
+        global new
+        new = tk.Toplevel(bg="green")
+        newlabel=tk.Label(new,text="PASS",fg="white")
+        newlabel.pack()
+        new.attributes('-fullscreen',True)
+        newbtn=tk.Button(new,text="OK",command=deleteInput)
+        newbtn.pack()
+    else:
+        new=tk.Toplevel(bg="red")
+        newlabel=tk.Label(new,text="Try Again",fg="white")
+        newlabel.pack()
+        new.attributes('-fullscreen',True)
+        newbtn=tk.Button(new,text="OK",command=deleteInput)
+        newbtn.pack()
 
+def deleteInput():
+    new.destroy()
+    textExample.delete(0,"end")
+    
+  
 textExample = tk.Entry(window, width=40)
 textExample.bind("<Return>",showInput)
 textExample.pack()
@@ -23,5 +43,8 @@ textExample.pack()
 
 label=tk.Label(window)
 label.pack()
+
+textlabel = tk.Label(window,text="Welcome",width=20,height=5,anchor="center",background="pink")
+textlabel.pack()
 
 window.mainloop()
